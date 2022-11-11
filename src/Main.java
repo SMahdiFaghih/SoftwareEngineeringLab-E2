4aac9736bc9e3b1022c61b769b51b7f92a4f63bc
mohammadreza <mohammad0reza79@gmail.com>
2022-11-11 12:24:48 +0330

    Add sample data to test functions

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,17 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+        int a = 24;
+        int b = 48;
+
+        System.out.println(add(a,b));
+
+        System.out.println(minus(a,b));
+
+        System.out.println(multipy(a,b));
+
+        System.out.println(divide(a,b));
+
     }
 
     private static int add(int a, int b)
