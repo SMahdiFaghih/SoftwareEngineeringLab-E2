fe2d7585c2e292e3422d62fe162830407b587543
mohammadreza <mohammad0reza79@gmail.com>
2022-11-11 12:09:10 +0330

    Add selection sort to project

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,29 @@
 public class Main
 {
+    public static void main(String[] args) {
+
+
+
+    }
+
+    private static void selectionSort(ArrayList<Integer> array)
+    {
+        for (int i = 0; i < array.size(); i++)
+        {
+            int minIndex = i;
+            for (int j = i + 1; j < array.size(); j++)
+            {
+                if (array.get(j) < array.get(minIndex))
+                {
+                    minIndex = j;
+                }
+            }
+
+            int temp = array.get(minIndex);
+            array.set(minIndex, array.get(i));
+            array.set(i, temp);
+        }
+    }
 }
+
+
