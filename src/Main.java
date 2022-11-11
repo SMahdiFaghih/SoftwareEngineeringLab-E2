777146fcba6c50337c411f2dacdae0dab5b7df31
SMahdiFaghih <smahdifaghih2001@gmail.com>
2022-11-11 12:02:04 +0330

    Add add & minus functions

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,17 @@
 public class Main
 {
+    public static void main(String[] args)
+    {
+
+    }
+
+    private static int add(int a, int b)
+    {
+        return a + b;
+    }
+
+    private static int minus(int a, int b)
+    {
+        return a - b;
+    }
 }
